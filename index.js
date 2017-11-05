1232b4736c84eacb496733c9c208fe5e8bfabc51
Samuel Gonzales <sgonz19@hotmail.com>
2017-11-05 02:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ if (name === "Joe") {
 } else {
     console.log("Expected the name to be Susan")
 }
-
+if (height=== "74"){
+    console.log("74")
+} else {
+    console.log(Expected height "74")
+}
 // Don't worry about this
 module.exports = { name, height
 }
